70bbd25946c46cca5028a97153df5b3fc6f135f6
jeonyeonbin <qtrgor@gmail.com>
2018-09-26 20:47:15 +0900

    상품 수정

--- public/controller/member/sell.controller.js
+++ public/controller/member/sell.controller.js
@@ -46,36 +46,63 @@ exports.showSellOneProduct = function(req,res){
         
         //hbs에는 JSON 형태로 보내줘야댐
         resolvedData = JSON.parse(resolvedData);
-        return res.render('member/showProductOne',{item:resolvedData});        
+        return res.render('member/showProductOne',{itemInfo:resolvedData,layout:'../shop/home-page'});        
     }).catch((e)=>{
       console.log(e);
     })
 
 }
 exports.updateProduct = function(req,res){
-     //등록하는사람의 아이디 + 현재시간 
-     var PicURL;
-     fileUpload(req).then(function(url){
-         PicURL = url;
-         return PicURL;   
-     }).then((PicURL)=>{
-        console.log(req.body.key);
-        console.log(req.body.itemName);
-        console.log(req.body.itemInfo);
-        console.log(req.body.itemPrice);
-        console.log(req.body.itemCategory);
-        var request = makeRequest('updateItem',[req.body.key,req.body.itemName,req.body.itemInfo,req.body.itemPrice,req.body.itemCategory,PicURL,PicURL]);
-             //url 정보 저장
-        return request;
-     }).then((request)=>{
-         FabricInvoke(request).then((resolvedData)=>{
-             return res.redirect(303,'/myPage');
-         }).catch((err)=>{
-                 console.log(err);
-         });
-     }).catch((err)=>{
-         console.log("file Error : "+err);
-     });
+    console.log('---------------------------------------------');
+    console.log('transactionMode'+ req.body.transactionMode);
+    console.log('transactionPosition '+ req.body.transactionPosition);
+    console.log('deliveryFee' + req.body.deliveryFee);
+    console.log('itemKey : '+ req.body.itemKey);
+    console.log('---------------------------------------------');
+    
+    // console.log(req.files);
+    /*
+    [Multer 초기화]
+    웹   서버에서 파일 업로드 버퍼를 처리하되 물리적으로 파일을 저장하지 않기 위해 메모리 스토리지 타입의 객체를 생성합니다.
+    웹 서버에 물리적으로 저장하는 것보다 아마존 S3 등을 이용하는 것이 좋은 이유는,
+    로드 밸런서 등을 활용하여 여러 웹 서버가 같은 Node.js 웹 서비스를 제공할 때 특정 서버만 파일을 보유하게 되는 현상 등이 생기는 것을 미연에 방지할 수 있기 때문입니다.
+    필요할 경우 메모리 스토리지 대신 다른 스토리지를 써도 됩니다.
+    */ 
+    //등록하는사람의 아이디 + 현재시간 
+    var PicURL;
+    if(req.body.transactionMode == '1'){
+        req.body.deliveryFee = '0';
+    }
+    console.log(req.body.deliveryFee);
+    fileUpload(req).then(function(url){
+        PicURL = url;
+        console.log("Promise : "+PicURL);
+        return PicURL;   
+    }).then((PicURL)=>{
+    // 0, itemKey
+	// 1, itemName
+	// 2, itemInfo
+	// 3, itemPrice
+	// 4, itemCategory
+	// 5, itemPic
+	// 6, itemContentsPic
+	// 7, sellState (1 : 판매중, 2 : 거래중, 3 : 판매완료, 4 : 판매취소)
+	// 8, transactionMode (1 : 직거래, 2 : 택배거래, 3 : 직거래 및 택배거래 모두 가능)
+	// 9, transactionPosition
+	// 10, deliveryFee (1 : 판매자부담, 2 : 착불)
+
+    var request = makeRequest('updateItem',[req.body.itemKey,req.body.itemName,req.body.itemInfo,req.body.itemPrice,req.body.itemCategory,PicURL,PicURL,req.body.sellState,req.body.transactionMode,req.body.transactionPosition,req.body.deliveryFee]);
+     console.log("Pic URL :"+PicURL);
+     return request;
+    }).then((request)=>{
+        FabricInvoke(request).then((resolvedData)=>{
+            return res.redirect(303,'/shopPage');
+        }).catch((err)=>{
+                console.log(err);
+        });
+    }).catch((err)=>{
+        console.log("file Error : "+err);
+    });
 }
 
 exports.updateDelivery = function(req,res){
