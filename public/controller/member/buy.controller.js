b40837adf18e8433aa415035378ce3d7444b8b84
jeonyeonbin <qtrgor@gmail.com>
2018-11-27 00:03:07 +0900

    buy controller error 수정

--- public/controller/member/buy.controller.js
+++ public/controller/member/buy.controller.js
@@ -8,16 +8,16 @@ var makeRequest = require('../returnRequest');
 exports.showBuyProductAllGET = function(req,res){
     var myId = req.session.name;
 
-
+    console.log('show buy Product All GET');
     console.log('my ID : '+myId);
     //세션 ID로 구매 상품 검색
     var request = makeRequest('queryTransactionInfoByBuyer',[myId]);
     var transInfoResult;                // 트랜잭션 정보들
     var itemKeyList = [];               // 아이템 키값 정보들
     var ItemResult;                     // 아이템 정보들
+
     FabricQuery(request).then((resolvedData)=>{
         transInfoResult = JSON.parse(resolvedData);
-        
         //ITEM Ke(y 정보들 수집
         transInfoResult.forEach(function(ele){
             if(itemKeyList.length == 0) itemKeyList.push(ele.itemKey);
@@ -28,22 +28,27 @@ exports.showBuyProductAllGET = function(req,res){
             }
         });
         
-
-        FabricQuery(makeRequest('queryItemByList',itemKeyList)).then((result)=>{
-            ItemResult = JSON.parse(result);
-            
-            transInfoResult.forEach(function(trans){
-                ItemResult.some(function(item){
-                    if(item.key == trans.itemKey){
-                    trans.item = item;
-                        return true;
-                    }
-                });
-            })
+        if(itemKeyList.length == 0){
+            console.log('here3');
             return res.render('member/buyProduct',{trans:transInfoResult,layout:'../shop/home-page'});
-        }).catch((err)=>{   
-            console.error(err);
-        })
+        }else{
+            FabricQuery(makeRequest('queryItemByList',itemKeyList)).then((result)=>{
+                ItemResult = JSON.parse(result);
+                
+                transInfoResult.forEach(function(trans){
+                    ItemResult.some(function(item){
+                        if(item.key == trans.itemKey){
+                        trans.item = item;
+                            return true;
+                        }
+                    });
+                })
+                return res.render('member/buyProduct',{trans:transInfoResult,layout:'../shop/home-page'});
+            }).catch((err)=>{   
+                console.error(err);
+            })
+        }
+       
     }).catch((e)=>{
         console.log(e);
         // return res.render('member/buyProduct',{layout:'../shop/home-page'});
